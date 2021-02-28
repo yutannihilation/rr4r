9fb7eb6ecd93378af26c2151bfb052cae72ad395
Hiroaki Yutani <yutani.ini@gmail.com>
2021-02-28 13:09:34 +0900

    Remove

--- src/rr4r/src/iter.rs
+++
@@ -1,6 +0,0 @@
-use extendr_api::prelude::*;
-use std::iter::{FromIterator, IntoIterator};
-
-impl FromIterator<Robj> for List {
-    fn from_iter<I: IntoIterator<Item = &str>>(iter: I) -> Self {}
-}
